70f581122bde10838efd511e7eb2509c0d491dc4
Nikita Zakharov <ii3axapobii@gmail.com>
2023-04-12 19:50:41 +0300

    Added program square comparing

--- Square/Program.cs
+++ Square/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Tap number");
+
+int a;
+if (int.TryParse(Console.ReadLine(), out a))
+{}
+else
+{
+    Console.WriteLine("OOOOps");
+}
+
+Console.WriteLine("Tap its sqare");
+int b;
+if (int.TryParse(Console.ReadLine(), out b))
+{}
+else
+{
+    Console.WriteLine("OOOOps");
+}
+
+if (a*a == b)
+{
+    Console.WriteLine("You are right");
+}
+else
+{
+    Console.WriteLine("Wrong");
+}
