f6623f9c103c1a562c13790b61941f0ed3bd2761
Nikita Zakharov <ii3axapobii@gmail.com>
2023-04-12 21:12:58 +0300

    More_or_less folder added

---
+++ More_or_less/Program.cs
@@ -0,0 +1,43 @@
+﻿//вводим два числа, находим меньшее, к меньшему прибавляем разницу между большим и меньшим столько раз, сколько равняется меньшее 
+int a;
+int b;
+int big_number;
+int small_number; 
+int result;
+
+Console.WriteLine("Enter the first number");
+if(int.TryParse(Console.ReadLine(), out a))
+{}
+else
+{
+    Console.WriteLine("You should enter the number");
+}
+
+Console.WriteLine("Enter the second number");
+if(int.TryParse(Console.ReadLine(), out b))
+{}
+else
+{
+    Console.WriteLine("You should enter the number");
+}
+
+if(a >= b)
+{
+    big_number = a;
+    small_number = b;
+}
+else
+{
+    big_number = b;
+    small_number = a;
+}
+
+int dif = big_number - small_number;
+int i = 0;
+result = small_number;
+while( i < small_number)
+{
+    result = result + dif;
+    i++;
+}
+Console.WriteLine(result);
