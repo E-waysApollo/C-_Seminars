c4d004eb3745edef042113589604b8873571d3cc
Nikita Zakharov <ii3axapobii@gmail.com>
2023-04-12 21:40:56 +0300

    Folder Natural_range added

---
+++ Natural_range/Program.cs
@@ -0,0 +1,18 @@
+﻿//enter the number and console write all range from negative number to positive/ ex: enter 2; write: -2, -1, 0, 1, 2
+
+Console.WriteLine("Enter the number");
+int a;
+if(int.TryParse(Console.ReadLine(), out a))
+{}
+else
+{
+  Console.WriteLine("Oooops");  
+}
+
+int n = a * -1;
+Console.Write(n);
+while( n < a)
+{
+    n++;
+    Console.Write($", {n}");
+}
