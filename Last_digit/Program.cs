660b1e333f45c8373ab1aa5945a86ce16df89acc
Nikita Zakharov <ii3axapobii@gmail.com>
2023-04-12 21:53:32 +0300

    Last_digit folder added

---
+++ Last_digit/Program.cs
@@ -0,0 +1,13 @@
+﻿//enter three-digit number. Programm should write the last digit
+
+Console.WriteLine("Enter three-digit number");
+int a;
+if(int.TryParse(Console.ReadLine(), out a) && a < 1000 && a >= 100)
+{
+    int remainder = a % 10;
+    Console.WriteLine(remainder);
+}
+else
+{
+    Console.WriteLine("Ooops");
+}
