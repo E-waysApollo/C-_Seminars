2c5e6783ef16f8a0e0e625c7887865ca0e06b24d
Nikita Zakharov <ii3axapobii@gmail.com>
2023-04-19 14:10:57 +0300

    task_6 added

---
+++ Homework/Task_6/Program.cs
@@ -0,0 +1,19 @@
+﻿//Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+Console.WriteLine("Enter the number");
+int num;
+if(int.TryParse(Console.ReadLine(), out num));
+
+else
+{
+    Console.WriteLine("Ooops");
+}
+int honest = num % 2;
+if(honest == 0)
+{
+    Console.WriteLine("honest");
+}
+else
+{
+    Console.WriteLine("not honest");
+}
