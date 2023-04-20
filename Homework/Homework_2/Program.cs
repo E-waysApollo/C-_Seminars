ea7eb351d76e215879d652830464c0a5c196918b
Nikita Zakharov <ii3axapobii@gmail.com>
2023-04-20 14:15:30 +0300

    main method added

--- Homework/Homework_2/Program.cs
+++ Homework/Homework_2/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int ReadNum(string argument)
+{
+    Console.WriteLine($"Input the {argument}");
+    int num;
+    while(!int.TryParse(Console.ReadLine(), out num))
+    Console.WriteLine("It's not a number");
+    return num;
+}
+
+void Main()
+{
+    bool IsWorking = true;
+    while(IsWorking)
+    {
+        Console.WriteLine("Input task number");
+        string task = Console.ReadLine;
+        switch(task)
+        {
+            case "10": task10(); break;
+            case "ex": IsWorking = false; break;
+            default: break;
+        }
+    }
+}
