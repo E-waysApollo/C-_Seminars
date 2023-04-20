d1bd5dd471db16a374479f8fb9aad2b706fff6ae
Nikita Zakharov <ii3axapobii@gmail.com>
2023-04-20 15:41:16 +0300

    task 10 added to homework 2 folder

--- Homework/Homework_2/Program.cs
+++ Homework/Homework_2/Program.cs
@@ -1,24 +1,41 @@
-﻿int ReadNum(string argument)
+﻿Main();
+
+int ReadNum(string argument)
 {
-    Console.WriteLine($"Input the {argument}");
+    Console.WriteLine($"Input {argument}");
     int num;
-    while(!int.TryParse(Console.ReadLine(), out num))
-    Console.WriteLine("It's not a number");
+    while (!int.TryParse(Console.ReadLine(), out num))
+        Console.WriteLine("It's not a number");
     return num;
 }
 
 void Main()
 {
     bool IsWorking = true;
-    while(IsWorking)
+    while (IsWorking)
     {
         Console.WriteLine("Input task number");
-        string task = Console.ReadLine;
-        switch(task)
+        string task = Console.ReadLine();
+        switch (task)
         {
             case "10": task10(); break;
             case "ex": IsWorking = false; break;
             default: break;
+        }
+    }
+}
+
+void task10()//Приграмма принимает на вход 3-х значное число и показывает 2-ю цифру числа
+{
+    bool ThreeDigit = false;
+    while (!ThreeDigit)
+    {
+        int num = ReadNum("three digit number");
+        if (num / 100 > 0 && num / 100 < 10)
+        {
+        num = (num - (num / 100 * 100) - (num % 10))/10;
+        Console.WriteLine($"Number is {num}");
+        ThreeDigit = true;
         }
     }
 }
