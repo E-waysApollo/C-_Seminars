307d9ef55632e28b500358e70acace4a5777728e
Nikita Zakharov <ii3axapobii@gmail.com>
2023-04-24 19:54:03 +0300

    Folder Seminar3_ReadNum method added

--- Seminar3/Program.cs
+++ Seminar3/Program.cs
@@ -1,5 +1,16 @@
 ﻿Main();
 
+int ReadNum(string argument)
+{
+    Console.WriteLine($"Input {argument}");
+    int num;
+    while(!int.TryParse(Console.ReadLine(), out num))
+    {
+        Console.WriteLine("It's not a number");   
+    }
+    return num;
+}
+
 void Main()
 {
     bool IsWoring = true;
