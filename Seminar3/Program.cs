3f9709df68eb422685db02d8154f58d175853c9b
Nikita Zakharov <ii3axapobii@gmail.com>
2023-04-24 21:03:25 +0300

    Folder Seminar3_method task21 added

--- Seminar3/Program.cs
+++ Seminar3/Program.cs
@@ -21,6 +21,7 @@ void Main()
         switch (taskNum)
         {
             case "18": task18(); break;
+            case "21": task21(); break;
             case "ex": IsWoring = false; break;
             default: break;
         }
@@ -29,3 +30,19 @@ void Main()
 
 void task18()
 {}
+
+void task21()//программа принимает на вход координаты точек х и у и находит расстояние между ними корень((х1-х2)^2 + (y1-y2)^2)
+{
+    int X1 = ReadNum("X1 ccordinate");
+    int Y1 = ReadNum("Y1 coordinate");
+    int X2 = ReadNum("X2 coordinate");
+    int Y2 = ReadNum("Y2 coordinate");
+    double xyDist = distance(X1, Y1, X2, Y2);
+    Console.WriteLine($"Diastance is {xyDist}");
+}
+
+double distance(int x1, int y1, int x2, int y2)
+{
+    double dist  = Math.Sqrt((x1-x2)*(x1-x2) + (y1-y2)*(y1-y2));
+    return dist;
+}
