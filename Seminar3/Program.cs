61cb9d7c178bfd6be835c8274af964c49e3f9dc6
Nikita Zakharov <ii3axapobii@gmail.com>
2023-04-24 19:40:53 +0300

    Folder Seminar3_Main method added

--- Seminar3/Program.cs
+++ Seminar3/Program.cs
@@ -1,1 +1,20 @@
-﻿
+﻿Main();
+
+void Main()
+{
+    bool IsWoring = true;
+    while(IsWoring)
+    {
+        Console.WriteLine("Input task number");
+        string taskNum = Console.ReadLine();
+        switch (taskNum)
+        {
+            case "18": task18(); break;
+            case "ex": IsWoring = false; break;
+            default: break;
+        }
+    }
+}
+
+void task18()
+{}
